33b1fa4e43ae47013a8484b254e90cd550455ae0
Dionmax Nobre <dionmax@edu.univali.br>
2019-08-11 00:52:11 -0300

    Seller Service & Find All

---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SellerService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        // Método não Async por hora.
+        public List<Seller> FindAll() 
+            => _context.Seller.ToList();
+    }
+}
